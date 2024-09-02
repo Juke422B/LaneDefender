3d52c7e8144b043313857a5b639eff17800ba1d7
Juke422B <jherron@mail.bradley.edu>
2024-09-02 08:46:20 -0500

    update

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-
+    public TMP_Text Score_Text;
+    private int Score;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
